3ba7236ac1ee8147608b987edf01fb767bb10d3d
suranjan <suranjanpradhan185@gmail.com>
2025-11-03 19:47:33 -0600

    Project Setup

---
+++ app/src/main/java/net/pradhan/vacationapp/UI/MainActivity.java
@@ -0,0 +1,39 @@
+package net.pradhan.vacationapp.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import net.pradhan.vacationapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VacationList.class);
+                intent.putExtra("test", "information sent");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/net/pradhan/vacationapp/UI/VacationList.java
@@ -0,0 +1,63 @@
+package net.pradhan.vacationapp.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import net.pradhan.vacationapp.R;
+
+public class VacationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vacation_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VacationList.this, VacationDetails.class);
+                startActivity(intent);
+            }
+        });
+        System.out.println(getIntent().getStringExtra("test"));
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.mysample) {
+            Toast.makeText(VacationList.this, "put in sample data", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if(item.getItemId()==android.R.id.home) {
+//            this.finish();
+            Intent intent = new Intent(VacationList.this, VacationDetails.class); //takes your across pages
+            startActivity(intent);
+            return true;
+        }
+        return true;
+    }
+}
